fa1d7ca47b3c080158510d971935000b478a1540
Surajudeen <aderojus@gmail.com>
2022-04-25 14:25:07 -0700

    homepage

--- src/components/pages/Homepage.vue
+++ src/components/pages/Homepage.vue
@@ -1,39 +1,47 @@
 <template>
     <div class="h-100 homepage__component">
         <!-- start changes here -->
-        <h1>Homepage content should be placed here</h1>
+        <h1>Creating <img src="@/assets/ring.svg">Product<img src="@/assets/left-arrow.svg">
+        <img src="@/assets/bud.svg">
+        With Outstanding Visual </h1>
+        <h3>and Great <img src="@/assets/elipse-shape.svg">Functionality</h3>
 
-        <h3>
-            Insert content and remove all dummy text
-        </h3>
+                
         <div>
             <p>
-                Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Ipsam aliquid, veritatis doloribus, iste corrupti deserunt voluptatum aut nobis fuga assumenda. Doloribus excepturi placeat non nulla dolorum quos iure ea reiciendis!
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet in ab cum inventore rem sequi sunt odio ipsa, quod asperiores quo nobis fugit dignissimos error! Laborum, commodi sequi dignissimos laboriosam.
+                        DeBrander is a multidisciplinary design agency that combines design, technology, art, and strategy to create state-of-the-art visual identity and experiences.         
             </p>
 
             <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo ea nisi eveniet illo architecto tenetur, dolorum quos maiores, impedit facilis temporibus, magnam vel sunt aliquid delectus corporis adipisci eaque quibusdam!
+                <img src="@/assets/contact-us.svg">
             </p>
 
             <div>
-                Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Laudantium nam illo facilis quaerat porro debitis voluptatum nulla enim consequuntur doloremque, sed cumque fugiat consequatur perspiciatis consectetur voluptas pariatur, autem ipsa!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi recusandae ullam ipsam ea enim culpa modi vitae sint, aut molestiae commodi blanditiis obcaecati porro sunt beatae optio. Officia, eum dignissimos.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi recusandae ullam ipsam ea enim culpa modi vitae sint, aut molestiae commodi blanditiis obcaecati porro sunt beatae optio. Officia, eum dignissimos.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi recusandae ullam ipsam ea enim culpa modi vitae sint, aut molestiae commodi blanditiis obcaecati porro sunt beatae optio. Officia, eum dignissimos.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi recusandae ullam ipsam ea enim culpa modi vitae sint, aut molestiae commodi blanditiis obcaecati porro sunt beatae optio. Officia, eum dignissimos.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi recusandae ullam ipsam ea enim culpa modi vitae sint, aut molestiae commodi blanditiis obcaecati porro sunt beatae optio. Officia, eum dignissimos.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi recusandae ullam ipsam ea enim culpa modi vitae sint, aut molestiae commodi blanditiis obcaecati porro sunt beatae optio. Officia, eum dignissimos.
+                
+                <img src="@/assets/sylvia-image.svg">
+
+               Clic UI/UX Design, Branding
+
+               Rainmaker UI/UX Design, Branding,
+                Development <button>Read Case Study</button>
+
+              
+              ADA
+              Compliance
+              UI/UX Design,
+              Branding
+
             </div>
 
             <div>
-                Lorem ipsum dolor sit, amet consectetur, adipisicing elit. Laudantium nam illo facilis quaerat porro debitis voluptatum nulla enim consequuntur doloremque, sed cumque fugiat consequatur perspiciatis consectetur voluptas pariatur, autem ipsa!
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi recusandae ullam ipsam ea enim culpa modi vitae sint, aut molestiae commodi blanditiis obcaecati porro sunt beatae optio. Officia, eum dignissimos.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi recusandae ullam ipsam ea enim culpa modi vitae sint, aut molestiae commodi blanditiis obcaecati porro sunt beatae optio. Officia, eum dignissimos.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi recusandae ullam ipsam ea enim culpa modi vitae sint, aut molestiae commodi blanditiis obcaecati porro sunt beatae optio. Officia, eum dignissimos.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi recusandae ullam ipsam ea enim culpa modi vitae sint, aut molestiae commodi blanditiis obcaecati porro sunt beatae optio. Officia, eum dignissimos.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi recusandae ullam ipsam ea enim culpa modi vitae sint, aut molestiae commodi blanditiis obcaecati porro sunt beatae optio. Officia, eum dignissimos.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi recusandae ullam ipsam ea enim culpa modi vitae sint, aut molestiae commodi blanditiis obcaecati porro sunt beatae optio. Officia, eum dignissimos.
+                Our clients are the companies and startups who make the world go round — they treat diseases, move parcels, insure cars, create jobs, send emails and publish courses.
+                Vast and complex businesses like these need digital experiences that are just as people-friendly as they are robust and scalable.
+                Through challenging core assumptions, we shape the products and services that improve the lives of thousands every single day.
+
+                 <button> About Us</button>
+<br>
+                We bring everything that’s <img src="@/assets/emoji-happy.svg">
+                required to build a product.
             </div>
         </div>
 
