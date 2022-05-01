c8029f92e7660d6f496efd56478a3e74a472e33e
Surajudeen <aderojus@gmail.com>
2022-05-01 09:10:50 -0700

    homepage

--- src/components/pages/Homepage.vue
+++ src/components/pages/Homepage.vue
@@ -1,51 +1,263 @@
 <template>
     <div class="h-100 homepage__component">
         <!-- start changes here -->
-        <h1>Creating <img src="@/assets/ring.svg">Product<img src="@/assets/left-arrow.svg">
-        <img src="@/assets/bud.svg">
-        With Outstanding Visual </h1>
-        <h3>and Great <img src="@/assets/elipse-shape.svg">Functionality</h3>
+        <h1 class=" display-3 mx-5">Creating <img style="height:50px" src="@/assets/ring.svg">
+            <span class="m-3">Products</span>
+            <img style="width:90px" src="@/assets/left-arrow.svg">
+            <img style="height:60px" src="@/assets/bud.svg"> 
+        </h1>
+        
+        <h1 class="display-3 mx-5" style="padding-left:150px">
+            <span class="font-italic"> With Outstanding</span>
+            <img style="height:40px" src="@/assets/elipse-shape.svg">
+            <span class="font-weight-light">Visual </span>
+        </h1>
+        <h3 class="display-3 mx-5" style="padding-left:300px">
+            <span class="font-weight-light">and Great</span> 
+            <img style="height:40px" src="@/assets/wave-shape.svg"> 
+            <span class=""> Functionality</span>
+        </h3>
 
                 
         <div>
-            <p>
-                        DeBrander is a multidisciplinary design agency that combines design, technology, art, and strategy to create state-of-the-art visual identity and experiences.         
-            </p>
+            <b-row>
+                <b-col cols-sm="12">
+                    <div class="d-flex">
+                        <p class="mb-0 m-5" style="padding-right:480px"> 
+                            <span class="" style="margin-left:50px">DeBrander is a multidisciplinary design agency 
+                            that</span> 
+                            combines <strong>design, technology, art,</strong> and <strong>strategy </strong> to create
+                            state-of-the-art visual identity and experiences.</p>         
+                        
 
-            <p>
-                <img src="@/assets/contact-us.svg">
-            </p>
+                        <span class="">
+                        <img class="contact-us my-5" style="height:150px; padding-right:200px" src="@/assets/contact-us.svg">
+                        </span>
+                    </div>
+                 </b-col>   
+            </b-row>
+            <div class="container">
+                <div class="second">
+                    
+                    <div class="d-flex">
+                        <div class="d-flex">
+                            <div class="syvia-image-container">
+                            <img  style="height:500px;" src="@/assets/sylvia-image.svg">
+                            </div>
+                            
+                            <div class="recent-work">
+                                <img style="height:80px; margin-top:100px;" src="@/assets/recent-works.svg">
+                            </div>
+                        </div>
 
-            <div>
-                
-                <img src="@/assets/sylvia-image.svg">
+                        <div class="d-flex m-5">
+                            <div class="" style="border:1px solid">
+                                <h4 class="m-3">Cylic</h4>
+                                <p class="mx-3" >UI/UX design, branding</p>
+                            </div>
+                            <div class="" style="background:#11271F; border:1px solid #11271F">
+                                <h4 class="m-3 text-white" >Rainmaker</h4>
+                                <p class="mx-5">UI/UX Design, Branding,Development</p>
+                                <a class="mx-3" href="/">Read case study <img src="@/assets/case-study-arrow.svg"></a>
+                            </div>
+                            <div cllass="" style="border:1px solid">
+                                <h4 class="m-3">ADA Compliance</h4>
+                                <p class="mx-3">UI/UX design, branding</p>
+                            </div>
 
-               Clic UI/UX Design, Branding
-
-               Rainmaker UI/UX Design, Branding,
-                Development <button>Read Case Study</button>
-
-              
-              ADA
-              Compliance
-              UI/UX Design,
-              Branding
-
-            </div>
-
-            <div>
-                Our clients are the companies and startups who make the world go round — they treat diseases, move parcels, insure cars, create jobs, send emails and publish courses.
-                Vast and complex businesses like these need digital experiences that are just as people-friendly as they are robust and scalable.
-                Through challenging core assumptions, we shape the products and services that improve the lives of thousands every single day.
-
-                 <button> About Us</button>
-<br>
-                We bring everything that’s <img src="@/assets/emoji-happy.svg">
-                required to build a product.
-            </div>
+                        </div>
+                    </div>
+                    
+                </div>
         </div>
+        <div class="overlay">
+            <b-row>
+                <b-col cols-sm="12">
+                `   <p class="m-5  pt-5 text-left justify-content-left" style="padding-right:100px"> 
+                    <span class="mx-5 mb-5" style="padding-left:50px">Our clients are the companies and startups who make the world go round — they treat diseases, move parcels, insure cars, create jobs, send emails and publish courses.
+                    <br><br>
+                    </span>
+                    
+                    Vast and complex businesses like these need digital experiences that are just as people-friendly as they are robust and scalable.<br>
+                    Through challenging core assumptions, we shape the products and services that improve the lives of thousands every single day.
+                
+                    <br>
+                    <b-button class="m-5 text-center" style="background:#11271F;" href="/">Our Approach</b-button>
+                    
+                    </p>
+                </b-col>
+            
+                <b-col cols-sm="12">
+                   <h1 class="display-4" style="padding-right:300px">We bring <br>everything<br> that’s <img style="height:40px; padding-right:px" src="@/assets/emoji-happy.svg">
+                        required<br> to build a product.
+                    </h1>
+                </b-col>
+            </b-row>
+        </div>
+        </div>
+        <div class="third-section">
+            <b-row>
+                <b-col cols-sm="12">
+                    <div class="d-flex m-3 pb-5 pt-5">
+                        <h1 class="mx-4 text-white" style="padding-right:63.5px"> The work we do,
+                        and The people we help.
+                        </h1>
+                        <p class="text-white mx-5" style="padding-left:190px"><span class="ten">10</span> — SaaS companies we’ve helped in the past 24 months. Our 
+                            holistic process takes care of discovery, branding, design, and development.
+                        </p>
+                    </div>
+                
+                        <div class="dashboard mx-5" style="margin-bottom:20px">
+                        <div class="background-image-container mx-5">
+                            <b-row>
+                                <b-col cols-sm="12">
+                                    <img  class="m-3" style="height:570px" src="@/assets/dashboard_image.svg">
+                                </b-col>
+                            </b-row>
 
+                            <b-row>
+                                <b-col cols-sm="12">
+                                    <a href="/"><img src="@/assets/Button.svg"></a>
+                                </b-col>
+                                <b-col cols-sm="12">
+                                     <q>
+                                        We are all storytellers. We all live in a network of stories. There isn’t a stronger connection between people than storytelling.” – Jimmy Neil Smith
+                                    </q>
+                                </b-col>
+
+                                </b-row>
+
+                                <b-row>
+                                    <b-col cols-sm="12">
+                                        <div style="position: relative; left:0; right:0;">
+                                        <img style="height:850px" class="img1 pt-5 pb-5" src="@/assets/christianal__image.svg">
+                                        
+                                        
+                                        <img class="img2 pb-5" style="height:400px"  src="@/assets/christiana_top_image.svg">
+                                         <h2 class="display-5 text-white text-left " style="position:absolute; z-index:1; left:500px; top:200px;"><span class="mx-3">Marshal — Creating a</span> dynamic and scalable design language to help power the future.</h2>
+                                         
+                                        <img class="client-logo" src="@/assets/clientforce-logo.svg">
+                                        <p class="ux-para">UI/UX Design, BrandingDevelopment</p>
+                                        <a href="/"><img  style="margin-top:600px" src="@/assets/Button.svg"></a> 
+                                        </div>
+                                    </b-col>
+                                   
+                                </b-row>
+                        </div>        
+                
+                </div>
+            </b-col>
+        </b-row>
     </div>
+    <div class="">
+        <b-row>
+            <b-col cols-sm="12">
+                <p class="m-5 text-center                                                                                                                                                                                                                                                                                                                                     display-4" style="padding-right:250px; padding-left:180px"> We translate research into solutions, crafting thoughtful and unified brands, apps, websites, interfaces, and systems.
+                    <img src="@/assets/wave-shapes.svg">
+                    <img src="@/assets/pet.svg">
+                </p>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col cols-sm="12">
+                <img style=" padding-left:100px; height:550px" src="@/assets/priscilla-image.svg">
+                <img style="height:450px; padding-left:50px" src="@/assets/brooke-cagle-image.svg">
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col cols-sm="12">
+              <p class="pt-5" style="padding-left:200px; padding-right:300px">
+                <img src="@/assets/left-arrow.svg">
+                We partner with design leaders of tech brands and growing businesses to create simple,
+                functional, and scalable digital solutions.
+                Our global team of researchers, strategists, creatives, and engineers work with streamlined 
+                      processes to break through organizational roadblocks and shape the brands of tomorrow.
+                </p>
+
+            </b-col>
+            
+        </b-row> 
+        <div class="m-5 text-center">
+             <a class="text-center pt-5 pb-5" href="#"><img style="height:60px" src="@/assets/button-2.svg"></a>
+        </div>
+    </div>
+        <div class="fifth-section">
+                <div class="fifth-section-background">
+                    <b-row>
+                
+                        <b-col cols-sm="12">
+                            <h3 class="font-weight-bold m-5 pt-5" style="padding-left:100px; padding-right:220px">How can we work together?</h3>
+                            <p class="" style="padding-left:150px; padding-right:200px">We’ve developed a simple onboarding process to help us better understand our client's strategic needs.</p>
+                        </b-col>   
+                        <b-col cols-sm="12">
+                            <img style="height:300px" src="@/assets/iamge 1.svg">
+                            <img  style="height:200px" src="@/assets/get-started.svg">
+                            <img class="mx-5" style="height:330px; padding-top:80px" src="@/assets/iamge 2.svg">
+
+                        </b-col> 
+                   </b-row>
+                </div>
+               <div class="last-sect"> 
+                   <div class="m-0  pt-0 last-sect-1">
+                        <b-row>
+                            <b-col cols-sm="12">
+                                 <img style="height:700px" src="@/assets/roberto-nickson-image.svg">
+                                 <img  style="height:50px" class="small-line" src="@/assets/small-lines.svg">
+                            </b-col>
+
+                            <b-col cols-sm="12">
+                              <div class="">
+                                <h1 class="m-5  font-weight-bold"><span class="mx-3"> Join Tech Mates to shape</span> experiences of tomorrow</h1>
+                                <p class="pr-5" style="margin-right:100px">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+                               <div class="pt-5  pb-5 text-center">
+                                <b-button href="/About" class=""  style="background:#11271F;">About us</b-button>
+                             </div>
+                                </div>
+                            </b-col>
+                        </b-row>
+                    </div>  
+                    <div class="d-flex inline m-5"  style="height:300px; padding-left:130px">
+                        <div class="" style="border-top:1px solid; border-bottom: 1px solid ">
+                            <p class="m-5" style=""> “I’d highly recommend TheBrander as a web design<br> agency, having worked with them on the design and<br> development of a new website over the past 3-4 <br>months. From initial brand development and<br> refinement, to the design and implementation of the <br>statements that it’s can’t be done! ”</p>
+                            <img class="table-image" style="height:40px; padding-left:400px" src="@/assets/table_image.svg">
+                        </div>
+
+                        <div class=""  style="border-left:1px solid; border-bottom:1px solid; border-top: 1px solid ">
+                            <p class="mx-5">Testimonial</p>
+                            <p class="mx-5" style="padding-top:180px">Williams Shakesspared</p>
+                            <p class="mx-5">- Founder of Being A Company</p>
+                        
+                        </div>
+
+                       
+                     
+                     </div>    
+                     
+                    <div class="">
+                        <b-row>
+                            <b-col cols-sm="12">
+                                <h1 class="m-5 font-weight-bold text-left justify-content-left" style="padding-right:500px; padding-left:200px">
+                                    <span class=""><img style="height:150px" src="@/assets/zigzag.svg"></span>
+                                    Have a project in mind? 
+                                    <span class="mx-5 mt-0">Let’s get to work.</span>
+                                </h1>
+                                <div class="mx-5" style="padding-left:250px">
+                                <b-button href="" style="background:#FEAC5E;color:#000">START A PROJECT</b-button>
+                                </div>
+                            </b-col>
+                        </b-row>
+                    
+                    </div>
+                   
+              </div> 
+        </div>
+        
+         <div>
+                    
+            <img  class="last-image" style="height:80px" src="@/assets/last-corner-image.svg">
+            </div>
+    </div>
+    
 </template>
 
 <script>
@@ -61,5 +273,104 @@ export default {
 <style lang="scss" scoped>
 .homepage__component {
     background: #fff;
+
 }
+.container{
+    position:relative
+}
+.overlay{
+    background: rgba(196, 196, 196, 0.5);
+    z-index:9;
+    margin-top:350px;
+    
+       
+
+    
+}
+
+.second{
+   position:absolute;
+   top:0;
+   left:0;
+   opacity:0.5
+   
+}
+.third-section{
+    background:#11271F;
+}
+.ten{
+    color:#FEAC5E;
+}
+.dashboard{
+    background: rgba(77, 77, 77, 0.27);
+    backdrop-filter: blur(80px);
+}
+.background-image-container{
+    background:url("@/assets/background-image.svg");
+    background-repeat:no-repeat;
+    background-size: 200px;
+    background-position: center;
+}
+q{
+    color:#11271F;
+}
+q::before {
+    content:open-quote;
+    color:white;
+    height:40px;
+    font:40px;
+}
+.img1{
+    position: relative;
+    top:0;
+    left:0;
+    z-index:1;
+}
+.img2{
+    position: absolute;
+    top:450px;
+    left:20px;
+    z-index:2
+}
+.client-logo{
+    position: relative;
+    top:200px
+}
+.ux-para{
+    position:absolute;
+    top:610px;
+    left:700px;
+    right:100px
+}
+.fifth-section{
+    background:#ffff
+}
+.fifth-section-background{
+    backdrop-filter: blur(80px);
+    background-image: url("@/assets/circl-background.svg");
+    background-repeat: no-repeat;
+    background-position: center;
+
+}
+  
+.small-line{
+    position:absolute; 
+    left:440px;
+    bottom:85px
+}
+.syvia-image-container{
+    position:relative;
+}
+.recent-work{
+    position:absolute;
+    left:400px;
+}
+
+ .last-image{
+     position:absolute;
+    /* top:6180px;*/
+     right:0
+ }
+
+
 </style>
